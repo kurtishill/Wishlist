2e686df8886c3a7bdc9d7042aae8f889384026ca
Kurtis Hill <kurtis.hill212@gmail.com>
2019-10-11 13:25:26 -0600

    fixed line alignment

--- Wishlist/ViewComponents/Grid/GridItemView.swift
+++ Wishlist/ViewComponents/Grid/GridItemView.swift
@@ -29,55 +29,55 @@ struct GridItemView<T: GridViewSelectDelegate>: View {
                 .animation(.spring())
                 .shadow(color: Color("lightBlue"), radius: 8, x: 0, y: 0)
                 .overlay(
-            GeometryReader { geometry in
-                VStack {
-                    HStack {
-                        Spacer()
-                        Image(systemName: self.gridViewDelegate.isItemFavorited(self.item) ? "heart.fill" : "heart")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(AssetColors.accentColor)
-                            .animation(.spring())
-                            .onTapGesture {
-                                self.gridViewDelegate.favoriteTapped(for: self.item)
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    Image(systemName: "gift.fill")
-                        .resizable()
-                        .frame(minWidth: 0, maxWidth: geometry.size.width - 80, minHeight: 0, maxHeight: geometry.size.height - 100)
-                        .foregroundColor(AssetColors.warningColor)
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Spacer()
-                            Text(self.item.name)
-                                .font(.custom(AssetsFonts.primaryFont, size: 25))
-                                .foregroundColor(.black)
-                                .minimumScaleFactor(0.8)
-                            Text(priceFormatter.string(from: self.item.price as NSNumber)!)
-                                .font(.custom(AssetsFonts.primaryFont, size: 15))
-                                .foregroundColor(.gray)
-                                .animation(.spring())
-                                .minimumScaleFactor(0.8)
-                            Spacer()
-                        }
-                        
-                        Spacer()
-                        
-                        Button(action: {
+                    GeometryReader { geometry in
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Image(systemName: self.gridViewDelegate.isItemFavorited(self.item) ? "heart.fill" : "heart")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(AssetColors.accentColor)
+                                    .animation(.spring())
+                                    .onTapGesture {
+                                        self.gridViewDelegate.favoriteTapped(for: self.item)
+                                }
+                            }
                             
-                        }) {
-                            Image(systemName: "ellipsis.circle.fill")
+                            Spacer()
+                            
+                            Image(systemName: "gift.fill")
                                 .resizable()
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(AssetColors.primaryColor)
-                        }
-                    }
-                }.padding(.all, 15)
-            })
+                                .frame(minWidth: 0, maxWidth: geometry.size.width - 80, minHeight: 0, maxHeight: geometry.size.height - 100)
+                                .foregroundColor(AssetColors.warningColor)
+                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Spacer()
+                                    Text(self.item.name)
+                                        .font(.custom(AssetsFonts.primaryFont, size: 25))
+                                        .foregroundColor(.black)
+                                        .minimumScaleFactor(0.8)
+                                    Text(priceFormatter.string(from: self.item.price as NSNumber)!)
+                                        .font(.custom(AssetsFonts.primaryFont, size: 15))
+                                        .foregroundColor(.gray)
+                                        .animation(.spring())
+                                        .minimumScaleFactor(0.8)
+                                    Spacer()
+                                }
+                                
+                                Spacer()
+                                
+                                Button(action: {
+                                    
+                                }) {
+                                    Image(systemName: "ellipsis.circle.fill")
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                        .foregroundColor(AssetColors.primaryColor)
+                                }
+                            }
+                        }.padding(.all, 15)
+                })
         }.offset(x: 2.5, y: 0)
             .offset(x: -2.5, y: 0)
     }
